21569fa8c562c51a91dad59330a0947a39f9a907
DavidParedesRodriguez <deividregalon2002@gmail.com>
2024-01-31 11:56:26 +0100

    ej1_primera_parte_hecho

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -20,3 +20,56 @@ Route::get('/', function () {
 Route::get('/posts', function () {
     return ('Listado de posts');
 });
+
+
+Route::get('fecha', function () {
+    return date("d/m/y h:i:s");
+});
+
+/*
+Route::get('saludo/{nombre}', function ($nombre) {
+    return "Hola, " . $nombre;
+});
+
+Route::get('saludo/{nombre?}', function ($nombre = "Invitado") {
+    return "Hola, " . $nombre;
+});
+
+
+Route::get('saludo/{nombre?}', function ($nombre = "Invitado") {
+    return "Hola, " . $nombre;
+})->where('nombre', "[A-Za-z]+");
+*/
+
+/*
+------------------------------------------------------------------------------------------------------------------
+Ejercicio 1
+
+Sobre el proyecto blog de la sesión anterior, vamos a añadir estos dos cambios:
+
+Añade una nueva ruta parametrizada a posts/{id} ,
+de manera que el parámetro id sea numérico (es decir, sólo contenga dígitos del 0 al 9) y obligatorio.
+Haz que la ruta devuelva el mensaje "Ficha del post XXXX", siendo XXXX el id que haya recibido como parámetro.
+
+*/
+
+Route::get('posts/{id}', function ($id) {
+    return "Ficha del post $id";
+})->where('id', "[0-9]")->name('posts_ficha');
+
+/*Pon un nombre a las tres rutas que hay definidas hasta ahora: a la página de inicio ponle el nombre "inicio",
+a la del listado la llamaremos "posts_listado" y a la de ficha que acabas de crear, la llamaremos "posts_ficha".*/
+
+Route::get('/', function () {
+    return "Página de Inicio";
+})->name('inicio');
+
+
+Route::get('listado', function () {
+    return "Página de listado";
+})->name('posts_listado');
+
+
+Route::get('/', function () {
+    return "Página de Inicio";
+})->name('inicio');
